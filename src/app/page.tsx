691dfcf9281968cc83a9f4f9477ffe25d4185635
Jiwon Choi <won0805@ewhain.net>
2025-04-29 21:19:15 +0900

    init: 초기 세팅

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <div className="font-pretendard font-bold">안녕 Pretendard Hello</div>
+      <div className="font-pretendard">안녕 Pretendard Hello</div>
+    </>
+  );
+}
